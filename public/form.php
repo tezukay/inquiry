7118585b773b646be154b8e3901f604426761257
tezukay <tezuka.yasu@gmail.com>
2018-05-08 18:37:17 +0900

    first commit

---
+++ public/form.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<h1>お問い合わせフォーム</h1>
+<form action="./input.php" method="post">
+お名前:<input name="name"><br>
+連絡先(email):<input name="address"><br>
+お問い合わせ内容
+<textarea name="body"></textarea><br>
+<button type="submit">お問い合わせ</button>
+</form>
---
+++ public/input.php
@@ -0,0 +1,6 @@
+<?php
+ob_start();
+
+
+
+header('Location: fin.html');
