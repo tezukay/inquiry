909212f7b4ef2e1ee5ddea5aa529da852772f805
tezukay <tezuka.yasu@gmail.com>
2018-05-15 18:37:49 +0900

    05/15

--- public/form.php
+++ public/form.php
@@ -1,10 +1,41 @@
 <?php
+ob_start();
+session_start();
+
+var_dump($_SESSION);
+if (isset($_SESSION['input'])){
+	$input_data = $_SESSION['input'];
+	unset($_SESSION['input']);
+}
+//erroeデータを取得
+$error = [];
+if (isset($_SESSION['error'])){
+	$error = $_SESSION['error'];
+	unset($_SESSION['error']);
+}
+//var_dump($input_data, $error);
+
+function h($s){
+	return htmlspecialchars($s, ENT_QUOTES);
+}
+
 ?>
 <h1>お問い合わせフォーム</h1>
+<?php
+if(true === isset($error['address_empty'])){
+	echo '連絡先は未入力です<br>';
+}
+
+if(true === isset($error['body_empty'])){
+	echo 'お問い合わせ内容は未入力です<br>';
+}
+
+?>
 <form action="./input.php" method="post">
-お名前:<input name="name"><br>
-連絡先(email):<input name="address"><br>
+お名前:<input name="name" value="<?php echo h(@$input_data['name']); ?>"><br>
+連絡先(email):<input name="address"
+	value="<?php echo h(@$input_data['address']); ?>"><br>
 お問い合わせ内容
-<textarea name="body"></textarea><br>
+<textarea name="body"><?php echo h(@$input_data['body']); ?></textarea><br>
 <button type="submit">お問い合わせ</button>
 </form>
--- public/input.php
+++ public/input.php
@@ -1,6 +1,51 @@
 <?php
 ob_start();
+session_start();
+
+//入力値の取得
+/* いろいろな｢入力値の取得方法｣`
+   $name =  (string)@$POST['name'];
+   $name = @$_POST['name'] ?? '';	//PHP7以降
+   $name = (string)filter_input(INPUT_POST, 'name');
+   if (true === isset($_POST['name'])) {
+	$name = $POST['name'];
+   } else{
+	$name = '';
+   }
+*/
+/* $name = @$_POST['name'] ?? '';
+   $address = @$_POST['name'] ?? '';
+   $body = @$_POST['name'] ?? '';
+*/
+
+$params = ['name', 'address', 'body'];
+$input_data = [];
+foreach($params as $p){
+	$input_data[$p] = $_POST[$p] ?? '';
+}
+//var_dump($input_data);
+
+//validata
+$error_flg = [];
+//address と bodyは必須入力
+if ('' === $input_data['address']){
+	//エラー
+	$error_flg['address_empty'] = 1;
+}
+if ('' === $input_data['body']){
+	//エラー
+	$error_flg['body_empty'] = 1;
+}
+if([] !== $error_flg){
+	$_SESSION['input'] = $input_data;
+	$_SESSION['error'] = $error_flg;
+	
+	//エラーが発生してる
+	header('Location: ./form.php');
+	exit;
+}
+//DBへのINSERT
 
 
-
+exit;
 header('Location: fin.html');
